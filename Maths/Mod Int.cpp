f47ed923ccaea222f3e5e502e63e3bd6c657f592
hvmegy <51798633+hamegy08lleo@users.noreply.github.com>
2024-10-01 13:10:48 +0700

    Update Mod Int.cpp

--- Maths/Mod Int.cpp
+++ Maths/Mod Int.cpp
@@ -11,7 +11,7 @@ template<int MOD, int RT> struct mint {
  	int v; 
  	explicit operator int() const { return v; } 
 	mint():v(0) {}
-	mint(ll _v):v(int(_v%MOD)) { v += (v<0)*MOD; }
+	mint(int _v):v((int)(_v%MOD)) { v += (v<0)*MOD; }
 	mint& operator+=(mint o) { 
 		if ((v += o.v) >= MOD) v -= MOD; 
 		return *this; }
@@ -19,13 +19,15 @@ template<int MOD, int RT> struct mint {
 		if ((v -= o.v) < 0) v += MOD; 
 		return *this; }
 	mint& operator*=(mint o) { 
-		v = int((ll)v*o.v%MOD); return *this; }
-	friend mint pow(mint a, ll p) { assert(p >= 0);
+		v = (int)((int)v*o.v%MOD); return *this; }
+	friend mint pow(mint a, int p) { assert(p >= 0);
 		return p==0?1:pow(a*a,p/2)*(p&1?a:1); }
 	friend mint inv(mint a) { assert(a.v != 0); return pow(a,MOD-2); }
 	friend mint operator+(mint a, mint b) { return a += b; }
 	friend mint operator-(mint a, mint b) { return a -= b; }
 	friend mint operator*(mint a, mint b) { return a *= b; }
 };
-using mi = mint<(int)1e9+7, 5>;
-using vmi = V<mi>;
+
+int const md = 998244353; 
+using mi = mint<md, 5>;
+
