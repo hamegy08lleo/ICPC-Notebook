b14e9b019a0380c31879ad39554607c55ae51d56
hvmegy <51798633+hamegy08lleo@users.noreply.github.com>
2024-10-17 17:37:45 +0700

    Update Mod Int.cpp
    change name because of conflict with segment tree implementation

--- Maths/Mod Int.cpp
+++ Maths/Mod Int.cpp
@@ -5,31 +5,29 @@
  * Usage: mi a = MOD+5; inv(a); // 400000003
  */
 
-template<int MOD> struct mint {
+template<int MOD> struct M {
 	static const int mod = MOD;
 	int v; 
 	explicit operator int() const { return v; } 
-	mint():v(0) {}
-	mint(int _v):v((int)(_v%MOD)) { v += (v<0)*MOD; }
-	mint& operator+=(mint o) { 
+	M():v(0) {}
+	M(int _v):v((int)(_v%MOD)) { v += (v<0)*MOD; }
+	M& operator+=(M o) { 
 		if ((v += o.v) >= MOD) v -= MOD; 
 		return *this; }
-	mint& operator-=(mint o) { 
+	M& operator-=(M o) { 
 		if ((v -= o.v) < 0) v += MOD; 
 		return *this; }
-	mint& operator*=(mint o) { 
+	M& operator*=(M o) { 
 		v = (int)((int)v*o.v%MOD); return *this; }
-	friend mint binpow(mint a, int p) { assert(p >= 0);
+	friend M binpow(M a, int p) { assert(p >= 0);
 		return p==0?1:binpow(a*a,p/2)*(p&1?a:1); }
-	friend mint inv(mint a) { assert(a.v != 0); return binpow(a,MOD-2); }
-	friend mint operator+(mint a, mint b) { return a += b; }
-	friend mint operator-(mint a, mint b) { return a -= b; }
-	friend mint operator*(mint a, mint b) { return a *= b; }
-	friend mint operator/(mint a, mint b) { return a *= inv(b); }
-	friend ostream & operator << (ostream & out, mint a) { return (out << a.v); }
+	friend M inv(M a) { assert(a.v != 0); return binpow(a,MOD-2); }
+	friend M operator+(M a, M b) { return a += b; }
+	friend M operator-(M a, M b) { return a -= b; }
+	friend M operator*(M a, M b) { return a *= b; }
+	friend M operator/(M a, M b) { return a *= inv(b); }
+	friend ostream & operator << (ostream & out, M a) { return (out << a.v); }
 };
 
 int const md = 1e9 + 7; 
-using mi = mint<md>;
-
-
+using mint = M<md>;
