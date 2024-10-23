417bbf3031219ff0ee5f907c795d847fc2453b50
binh1922004 <147068448+binh1922004@users.noreply.github.com>
2024-10-23 14:40:08 +0700

    Update Hash.cpp

--- Strings/Hash.cpp
+++ Strings/Hash.cpp
@@ -1,21 +1,54 @@
-int base = 311;
-int const MOD = 1000000007;
-int hashS[MAX];
-int POW[MAX];
-int gethash(int i, int j, int hashS[MAX]){
-    return (hashS[j] - hashS[i - 1] * POW[j - i + 1] + MODMOD) % MOD;
-}
-void setHash(){
-    string s;
-    n = s.length();
-    POW[0] = 1;
-    s = " " + s;
-    t = " " + t;
-    for (int i = 1; i <= n; i++)
-        POW[i] = (base POW[i - 1])%MOD;
-    for (int i = 1; i <= n; i++){
-        hashS[i] = (hashS[i - 1] * base + s[i]) % MOD;
-    }
-    for (int i = 1; i <= m; i++)
-        hashT[i] = (hashT[i - 1] * base + t[i]) % MOD;
-}
+struct hashing{
+        int mod, n;
+        vector<int> h, k, power;
+        int binpow(int a, int n){
+                if (n == 0)
+                        return 1LL;
+                int res = binpow(a, n/2);
+                if (n % 2)
+                        return res * res % mod * a % mod;
+                return res * res % mod;
+        }
+        hashing() {}
+        hashing(string s, int mod): mod(mod){
+                this->n = s.length();
+                power = k = h = vector<int> (n);
+                power[0] = 1;
+                h[0] = s[0];
+                for (int i = 1; i < n; i++){
+                        power[i] = power[i - 1] * base % mod;
+                        h[i] = (h[i - 1] * base + s[i]) % mod;
+                }
+                reverse(all(s));
+                k[0] = s[0];
+                for (int i = 1; i < n; i++){
+                        k[i] = (k[i - 1] * base + s[i]) % mod;
+                }
+        }
+
+        int getH(int l, int r){
+                return (!l ? h[r] : (h[r] - h[l - 1] * power[r - l + 1] + mod * mod) % mod);
+        }
+        int getK(int l, int r){
+                int L = l;
+                l = n - 1 - r;
+                r = n - 1 - L;
+                return (!l ? k[r] : (k[r] - k[l - 1] * power[r - l + 1] + mod * mod) % mod);
+        }
+};
+
+struct bighash{
+        bighash() {}
+        hashing a, b;
+        bighash(string s){
+                a = hashing(s, mod1);
+                b = hashing(s, mod2);
+        }
+
+        int getH(int l, int r){
+                return a.getH(l, r) * b.getH(l, r);
+        }
+        int getK(int l, int r){
+                return a.getK(l, r) * b.getK(l, r);
+        }
+};
