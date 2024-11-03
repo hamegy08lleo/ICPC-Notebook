e9927b848355351ef1214cba55950db52a1c602f
hvmegy <51798633+hamegy08lleo@users.noreply.github.com>
2024-11-03 15:59:56 +0700

    Update TwoSatSolver.cpp

--- Graphs/TwoSatSolver.cpp
+++ Graphs/TwoSatSolver.cpp
@@ -1,66 +1,49 @@
-struct TSS { 
-	int nvar; 
-	int nvrt; 
-	vector<vector<int>> adj;
-	vector<int> res, scc, topo, vis, in, low, del, idx; 
-
-	TSS(int nvar):
-		nvar(nvar), 
-		nvrt(nvar * 2), 
-		adj(nvrt + 1), 
-		res(nvar + 1), 
-		scc(nvrt + 1), 
-		in(nvrt + 1), 
-		low(nvrt + 1), 
-		del(nvrt + 1), 
-		idx(nvrt + 1) {}
-
-	int conj(int u) { 
-		if (u > nvar) return u - nvar; 
-		return u + nvar;
-	}
-	void edge(int u, bool nu, int v, bool nv) { 
-		if (nu) u = conj(u); 
-		if (nv) v = conj(v); 
-		adj[u].push_back(v); 
-	}
-	int curdfs = 0; 
-	stack<int> st; 
-	int curidx = nvrt; 
-	void tarjan(int u) { 
-		in[u] = low[u] = ++curdfs; 
-		st.push(u); 
-		for (int v : adj[u]) { 
-			if (del[v]) continue; 
-			if (!in[v]) { 
-				tarjan(v); 
-				low[u] = min(low[u], low[v]); 
-			}
-			else { 
-				low[u] = min(low[u], in[v]); 
-			}
-		}
-		if (low[u] == in[u]) { 
-			idx[u] = curidx--; ; 
-			while (st.top() != u) { 
-				int v = st.top(); 
-				st.pop(); 
-				del[v] = true; 
-				scc[v] = u; 
-			}
-			scc[u] = u; 
-			del[u] = true; 
-			st.pop(); 
-		}
-	}
-	bool solve() { 
-		for (int i = 1; i <= nvrt; i++) { 
-			if (!in[i]) tarjan(i); 
-		}
-		for (int i = 1; i <= nvar; i++) { 
-			if (scc[i] == scc[conj(i)]) return false; 
-			res[i] = idx[scc[i]] > idx[scc[conj(i)]]; 
-		}
-		return true; 
-	}
+struct TSS {
+        int nvar, nvrt;
+        vector<vector<int>> adj;
+        vector<bool> res;
+        TSS(int nvar): nvar(nvar), nvrt(nvar * 2), adj(nvrt + 1), res(nvar + 1) {}
+        int conj(int u) {
+                if (u > nvar) return u - nvar;
+                return u + nvar;
+        }
+        int operator[](int i) { return res[i]; }
+        void add(int u, bool nu, int v, bool nv) {
+                if (nu) u = conj(u);
+                if (nv) v = conj(v);
+                adj[u].push_back(v);
+        }
+        bool solve() {
+                vector<int> in(nvrt + 1), low(nvrt + 1), id(nvrt + 1);
+                stack<int> st;
+                int curdfs = 0, curidx = 2 * nvrt + 1;
+                auto tarjan = [&](auto&& f, int u) -> void {
+                        in[u] = low[u] = ++curdfs;
+                        st.push(u);
+                        for (int v : adj[u]) {
+                                if (!in[v]) {
+                                        f(f, v);
+                                        low[u] = min(low[u], low[v]);
+                                } else if (!id[v]) {
+                                        low[u] = min(low[u], in[v]);
+                                }
+                        }
+                        if (low[u] == in[u]) {
+                                int v;
+                                do {
+                                        v = st.top();
+                                        st.pop();
+                                        id[v] = curidx;
+                                } while (v != u);
+                                --curidx;
+                        }
+                };
+                for (int i = 1; i <= nvrt; i++)
+                        if (!in[i]) tarjan(tarjan, i);
+                for (int i = 1; i <= nvar; i++) {
+                        if (id[i] == id[conj(i)]) return false;
+                        res[i] = id[i] > id[conj(i)];
+                }
+                return true;
+        }
 };
