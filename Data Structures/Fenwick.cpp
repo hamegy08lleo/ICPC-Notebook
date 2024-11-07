6b9af720fccecb186823557b58fa7c479e5574b5
hvmegy <hamegy08lleo@gmail.com>
2024-11-07 09:05:13 +0700

    modify

--- Data Structures/Fenwick.cpp
+++ Data Structures/Fenwick.cpp
@@ -1,25 +1,25 @@
 template<typename T> 
 struct fenwick { 
-        vector<T> f; 
-        int n; 
-        fenwick(int n): n(n) { 
-                f.resize(n + 1); 
-        }
-        void add(int i, T v) { 
-                dbg(i); 
-                for (; i <= n; i += i & -i) {
-                    f[i] += v; 
-                    dbg(i, v); 
-                }
-        }
-        T get(int i) { 
-                T ans = 0; 
-                for (; i >= 1; i -= i & -i) ans += f[i]; 
-                return ans; 
-        }
-	    T find(T k) {
+	vector<T> f; 
+	int n; 
+	fenwick(int n): n(n) { 
+		f.resize(n + 1); 
+	}
+	void add(int i, T v) { 
+		dbg(i); 
+		for (; i <= n; i += i & -i) {
+			f[i] += v; 
+			dbg(i, v); 
+		}
+	}
+	T get(int i) { 
+		T ans = 0; 
+		for (; i >= 1; i -= i & -i) ans += f[i]; 
+		return ans; 
+	}
+	T find(T k) {
 		T sum = 0;
-        int pos = 0;
+		int pos = 0;
 		for (int i = __lg(n); i >= 0; i--)
 		{
 			if (pos + (1 << i) < n && sum + f[pos + (1 << i)] < k)
--- Graphs/Bridge.cpp
+++ Graphs/Bridge.cpp
@@ -1,15 +1,1 @@
-void dfs(int v, int p = -1) {
-    visited[v] = true;
-    tin[v] = low[v] = timer++;
-    for (int to : adj[v]) {
-        if (to == p) continue;
-        if (visited[to]) {
-            low[v] = min(low[v], tin[to]);
-        } else {
-            dfs(to, v);
-            low[v] = min(low[v], low[to]);
-            if (low[to] > tin[v])
-                IS_BRIDGE(v, to);
-        }
-    }
-}
+low[u] = num[u]
