8f41709499e9b1ba376842c04aa1ba2eace21b8b
hvmegy <51798633+hvmegy@users.noreply.github.com>
2024-11-18 12:24:11 +0700

    Update Block-Cut Tree.cpp

--- Graphs/Block-Cut Tree.cpp
+++ Graphs/Block-Cut Tree.cpp
@@ -1,16 +1,12 @@
-
 /** @return the block-cut tree of a graph */
 // 0-indexed, number of block can be up to 3 * n
-vector<vector<int>> biconnected_components(vector<vector<int>> &g,
-                                           vector<bool> &is_cutpoint, vector<int> &id) {
+vector<vector<int>> build_bcc(vector<vector<int>> &g, vector<bool> &is_cutpoint, vector<int> &id) {
 	int n = (int)g.size();
 	vector<vector<int>> comps;
 	vector<int> stk;
 	vector<int> num(n);
 	vector<int> low(n);
-
 	is_cutpoint.resize(n);
-
 	// Finds the biconnected components
 	function<void(int, int, int &)> dfs = [&](int node, int parent, int &timer) {
 		num[node] = low[node] = ++timer;
@@ -33,13 +29,9 @@ vector<vector<int>> biconnected_components(vector<vector<int>> &g,
 			}
 		}
 	};
-
 	int timer = 0;
-
 	dfs(0, -1, timer);
-
 	id.resize(n);
-
 	// Build the block-cut tree
 	function<vector<vector<int>>()> build_tree = [&]() {
 		vector<vector<int>> t(1);
@@ -50,7 +42,6 @@ vector<vector<int>> biconnected_components(vector<vector<int>> &g,
 				t.push_back({});
 			}
 		}
-
 		for (auto &comp : comps) {
 			int node = node_id++;
 			t.push_back({});
@@ -63,8 +54,6 @@ vector<vector<int>> biconnected_components(vector<vector<int>> &g,
 				}
 		}
 		return t;
-
 	};
 	return build_tree();
 }
-
