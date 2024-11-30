2d52055063a88b3fe54087f81db436cc9b50345c
binh1922004 <147068448+binh1922004@users.noreply.github.com>
2024-11-30 23:17:38 +0700

    Update Z Function.cpp

--- Strings/Z Function.cpp
+++ Strings/Z Function.cpp
@@ -1,10 +1,13 @@
-z[0] = n;
+vector<int> z_func(string s){
+    int n = s.length();
+    vector<int> z(n);
+    z[0] = n;
     int l = 0, r = 0;
     for (int i = 1; i < n; i++){
         if (i > r){
             l = r = i;
             while (r < n && s[r] == s[r-l]) ++r;
-            z[i] = r - l; 
+            z[i] = r - l;
             r-=1;
         }
         else{
@@ -18,3 +21,5 @@ z[0] = n;
             }
         }
     }
+    return z;
+}
