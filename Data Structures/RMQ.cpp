3745d1cb0c8e372ae7bfe970aab66f486d90cf39
hvmegy <51798633+hvmegy@users.noreply.github.com>
2024-12-06 01:09:28 +0700

    Update RMQ.cpp

--- Data Structures/RMQ.cpp
+++ Data Structures/RMQ.cpp
@@ -1,22 +1,24 @@
+// 0-indexed
+template<typename T>
 struct RMQ { 
         int n, lg;
-        vector<int> v; 
-        vector<vector<int>> st; 
-        RMQ(vector<int> & v): v(v), n(v.size() - 1) { 
+        vector<T> v; 
+        vector<vector<T>> st; 
+        function<T(T, T)> f; 
+        RMQ(vector<T> & v, function<T(T, T)> f = [](T i, T j) { return min(i, j); }): v(v), n(v.size()), f(f) { 
                 lg = __lg(n) + 1; 
-                st.resize(n + 1, vector<int> (lg + 1)); 
-                for (int i = 1; i <= n; i++) { 
+                st.resize(n, vector<int> (lg + 1)); 
+                for (int i = 0; i < n; i++) { 
                         st[i][0] = v[i]; 
                 }
                 for (int i = 1; i <= lg; i++) { 
-                        for (int j = 1; j + (1LL << i) - 1 <= n; j++) { 
-                                st[j][i] = min(st[j][i - 1], st[j + (1LL << (i - 1))][i - 1]); 
+                        for (int j = 0; j + (1LL << i) - 1 < n; j++) { 
+                                st[j][i] = f(st[j][i - 1], st[j + (1LL << (i - 1))][i - 1]); 
                         }
                 }
         }
         int get(int l, int r) { 
-                assert(l <= r);
                 int len = __lg(r - l + 1); 
-                return min(st[l][len], st[r - (1 << len) + 1][len]); 
+                return f(st[l][len], st[r - (1 << len) + 1][len]); 
         }
 }; 
