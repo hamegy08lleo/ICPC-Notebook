7de9bec12bb9813ed6b342863ddc7026b58e4303
hvmegy <51798633+hvmegy@users.noreply.github.com>
2024-12-06 21:45:18 +0700

    Update TwoSatSolver.cpp

--- Graphs/TwoSatSolver.cpp
+++ Graphs/TwoSatSolver.cpp
@@ -2,43 +2,40 @@ struct TSS {
         int nvar, nvrt;
         vector<vector<int>> adj;
         vector<bool> res;
-        TSS(int nvar): nvar(nvar), nvrt(nvar * 2 + 2), adj(nvrt + 1), res(nvar + 1) {}
-
+        TSS(int nvar)
+            : nvar(nvar), nvrt(nvar * 2 + 2), adj(nvrt + 1), res(nvar + 1) {}
+ 
         int operator[](int i) { return res[i]; }
-	// nu ? !u : u -> !nv ? !v : v
+        // nu ? !u : u -> !nv ? !v : v
         void add(int u, bool nu, int v, bool nv) {
-		u <<= 1; 
-		v <<= 1; 
-                u ^= nu; 
-		v ^= nv; 
-                adj[u].push_back(v);
+		adj[u << 1 | nu].push_back(v << 1 | nv); 
         }
-	int addvar() { 
-		nvar++; 
-		nvrt += 2; 
-		adj.push_back({});
-		adj.push_back({});  
-		res.push_back({});
-		return nvar; 
-	}
-	void atmostone(vector<int> & var) { 
-		if (var.size() == 1) return; 
-		int pre = addvar(); 
-		add(var[0], 0, pre, 0); 
-		add(var[0], 1, pre, 1); 
-		add(pre, 0, var[0], 0); 
-		add(pre, 1, var[0], 1); 
-		for (int i = 1; i < var.size(); i++) { 
-			add(var[i], 0, pre, 1); 
-			add(pre, 0, var[i], 1); 
-			int cur = addvar(); 
-			add(var[i], 0, cur, 0); 
-			add(pre, 0, cur, 0); 
-			add(cur, 1, var[i], 1); 
-			add(cur, 1, pre, 1); 
-			pre = cur; 
-		}
-	}
+        int addvar() {
+                nvar++;
+                nvrt += 2;
+                adj.push_back({});
+                adj.push_back({});
+                res.push_back({});
+                return nvar;
+        }
+        void atmostone(vector<int>& var) {
+                if (var.size() <= 1) return;
+                int pre = addvar();
+                add(var[0], 0, pre, 0);
+                add(var[0], 1, pre, 1);
+                add(pre, 0, var[0], 0);
+                add(pre, 1, var[0], 1);
+                for (int i = 1; i < var.size(); i++) {
+                        add(var[i], 0, pre, 1);
+                        add(pre, 0, var[i], 1);
+                        int cur = addvar();
+                        add(var[i], 0, cur, 0);
+                        add(pre, 0, cur, 0);
+                        add(cur, 1, var[i], 1);
+                        add(cur, 1, pre, 1);
+                        pre = cur;
+                }
+        }
         bool solve() {
                 vector<int> in(nvrt + 1), low(nvrt + 1), id(nvrt + 1);
                 stack<int> st;
@@ -67,8 +64,8 @@ struct TSS {
                 for (int i = 1; i <= nvrt; i++)
                         if (!in[i]) tarjan(tarjan, i);
                 for (int i = 1; i <= nvar; i++) {
-                        if (id[i] == id[i ^ 1]) return false;
-                        res[i] = id[i] > id[i ^ 1];
+                        if (id[i << 1] == id[i << 1 ^ 1]) return false;
+                        res[i] = id[i << 1] > id[i << 1 ^ 1];
                 }
                 return true;
         }
